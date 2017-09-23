b8a38790b6c772e3bf0358ae55b390fb440706f7
Gabriel Zarate <gabriel@vertmob.com>
2017-09-23 08:51:42 -0400

    react animations working

--- src/actions/animations.js
+++ src/actions/animations.js
@@ -6,3 +6,24 @@ export function playHomeAnimation(bool){
 		bool
 	}
 }
+
+
+// const duration = 1;
+// const backgroundMainLeft = document.querySelector(".background-piece-left-main");
+// const backgroundColorLeft = document.querySelector(".background-piece-left-main");
+// const backgroundColorRight = document.querySelector(".background-piece-right-color");
+
+
+// export default {
+// 	show(target, cb){
+// 		return TimelineMax({
+// 			.to(backgroundColorLeft, 1.2, {
+// 				scaleX: 1,
+// 				ease: Expo.easeInOut
+// 			}, 0).to(backgroundColorRight)
+// 			onComplete: function e(){
+// 				cb();
+// 			}
+// 		})
+// 	}
+// }
