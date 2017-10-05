d7e118c19d90ed10ad64e43201301c46df65d072
Gabriel Zarate <gabriel@vertmob.com>
2017-10-05 18:33:56 -0400

    project content setup

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,11 +5,11 @@ class Project extends Component {
   render() {
   	const data = this.props.project;
     return (
-        <section className="standard-section-padding project-section">
+        <section className="standard-section-padding no-padding-bottom project-section" style={{ backgroundImage: data[0].projectImgLg }}>
 
     		<div className="flex-row full-height bottom-xs">
 
-    			<div className="flex-col flex-half">
+    			<div className="flex-col flex-two-thirds project-single-details">
         			<h4>{data[0].projectDescription}</h4>
         			<div className="project-details">
         				<p>Agency: {data[0].projectAgency}</p>
--- src/content.js
+++ src/content.js
@@ -2,6 +2,7 @@ import placeholderCardImage from './assets/production/images/project-thumbnail.j
 import sponsorCardImage from './assets/production/images/sportsengine-sponsor-image_resized.jpg';
 import sweetwaterVideo from './assets/production/video/swb-homepage-video.mp4';
 import analyzeCardImage from './assets/production/images/project-thumbnail_analyze.png';
+import sweetwaterLg from './assets/production/images/wtwtsw-callout.jpg';
 
 export const projects = [
 	{
@@ -9,189 +10,54 @@ export const projects = [
 		projectName: "SweetWater Brewing",
 		projectURL: "sweetwater",
 		projectCategory: "Web Development",
-		projectDescription: "Turn unused real estate on your website into dynamic profit centers that connect sponsors with your club’s members.",
-		projectIcon: "icon-website-big",
+		projectDescription: "A highly interactive, branded site redesign for SweetWater Brewery.",
 		cardImage: null,
 		cardVideo: sweetwaterVideo,
-		projectColor: '#26356b',
-		projectImage: "images/project-image-1.jpg",
-
+		projectColor: '#128879',
+		projectImgLg: sweetwaterLg,
+		projectLink: 'http://sweetwaterbrew.com/',
+		technologyUsed: 'SASS, Flexbox, Swiper, Wordpress',
 		projectRole: 'Conception, Art Direction, Web Design, Responsive Design',
 		projectTeam: 'Jason Lehmkuhle',
 		projectAgency: 'Vert Digital',
 		projectDate: '2017',
 		projectImageDescription: "Website Sponsorship project",
 		additionalInfo: {},
-		projectOptions: [
-			{
-				name: 'Banner Ad',
-				selected: false,
-				amount: '100',
-				details: ['Page-width ad with image/logo. Hyperlinks to a designated URL.']
-			},
-			{
-				name: 'CTA Ad',
-				selected: false,
-				amount: '60',
-				details: ['1/3-page square ad with image/logo. Hyperlinks to a designated URL.']
-			},
-			{
-				name: 'Sidebar Logo',
-				selected: false,
-				amount: '40',
-				frequency: 'monthly',
-				details: ['Logo only. Hyperlinks to a designated URL.']
-			}
-		],
-		discount: {},
-		revenueEst: 0
 	},
 	{
 		id: 2,
 		projectName: "Sponsorship App",
 		projectURL: "sponsor",
 		projectCategory: "React / Redux Project",
+		projectImgLg: sweetwaterLg,
+		projectLink: 'http://inspire.sportsengine.com/',
 		projectDescription: "Turn your club’s emails into win-win opportunities by allowing sponsors to provide exclusive offers to your members.",
 		projectAgency: 'Vert Digital',
 		projectRole: 'Conception, Art Direction, Web Design, Responsive Design',
 		projectTeam: 'Jason Lehmkuhle',
 		projectDate: '2017',
 		cardImage: sponsorCardImage,
-		projectColor: '#26356b',
-		projectIcon: "icon-email-big",
-		projectImage: "images/project-image-2.jpg",
-		projectImageDescription: "Email Sponsorship project",
-		additionalInfo: {},
-		projectOptions: [
-			{
-				name: 'Banner ad',
-				selected: false,
-				amount: '100',
-				details: [
-					'Page-width ad with image/logo. Hyperlinks to a designated URL.',
-					'Include a message and offer in the email text to be sent directly to our members.'
-				]
-			}
-		],
-		discount: {},
-		revenueEst: 0
+		projectColor: 'rgba(40,165,225, 0.8)',
 	},
 	{
 		id: 3,
 		projectName: "Analyze",
 		projectURL: "analyze",
 		projectCategory: "React App for Therapists",
+		projectLink: 'http://gabrielzzarate.github.io/analyze-finalproject/',
 		projectDescription: "Put your sponsors front and center with value-add opportunities during your club’s registration process this season.",
-		projectIcon: "icon-registration",
 		cardImage: analyzeCardImage,
-		projectColor: '#26356b',
+		projectImgLg: sweetwaterLg,
+		projectColor: 'rgba(38,53,107,0.8)',
 		projectImage: "images/project-image-3.jpg",
-		projectImageDescription: "Registration Sponsorship project",
+		projectImageDescription: "SportsEngine Sponsorship App",
 		projectAgency: 'Vert Digital',
 		projectRole: 'Conception, Art Direction, Web Design, Responsive Design',
 		projectTeam: 'Jason Lehmkuhle',
 		projectDate: '2017',
-		additionalInfo: {},
-		projectOptions: [
-			{
-				name: 'Web page banner ad & email confirmation ad',
-				selected: false,
-				amount: '100',
-				frequency: "monthly",
-				details: [
-					'Includes: 1 Page-width web page ad with image/logo. Hyperlinks to a designated URL 1 Page-width email ad with image/logo. Hyperlinks to a designated URL.',
-					'Include a message and offer in the email text to be sent directly to our members'
-				]
-			}
-		],
-		discount: {},
-		revenueEst: 0
 	},
 ];
 
-export const about = [
-	{
-		projectName: "SweetWater Brewing",
-		projectURL: "sweetwater",
-		projectCategory: "Web Development",
-		id: 1,
-		projectDescription: "Turn unused real estate on your website into dynamic profit centers that connect sponsors with your club’s members.",
-		projectIcon: "icon-website-big",
-		projectImage: "images/project-image-1.jpg",
-		projectImageDescription: "Website Sponsorship project",
-		billingFrequency: 'Monthly',
-		additionalInfo: {},
-		projectOptions: [
-			{
-				name: 'Banner Ad',
-				selected: false,
-				amount: '100',
-				details: ['Page-width ad with image/logo. Hyperlinks to a designated URL.']
-			},
-			{
-				name: 'CTA Ad',
-				selected: false,
-				amount: '60',
-				details: ['1/3-page square ad with image/logo. Hyperlinks to a designated URL.']
-			},
-			{
-				name: 'Sidebar Logo',
-				selected: false,
-				amount: '40',
-				frequency: 'monthly',
-				details: ['Logo only. Hyperlinks to a designated URL.']
-			}
-		],
-		discount: {},
-		revenueEst: 0
-	},
-	{
-		projectName: "Email",
-		id: 2,
-		projectDescription: "Turn your club’s emails into win-win opportunities by allowing sponsors to provide exclusive offers to your members.",
-		projectIcon: "icon-email-big",
-		projectImage: "images/project-image-2.jpg",
-		projectImageDescription: "Email Sponsorship project",
-		billingFrequency: 'Monthly',
-		additionalInfo: {},
-		projectOptions: [
-			{
-				name: 'Banner ad',
-				selected: false,
-				amount: '100',
-				details: [
-					'Page-width ad with image/logo. Hyperlinks to a designated URL.',
-					'Include a message and offer in the email text to be sent directly to our members.'
-				]
-			}
-		],
-		discount: {},
-		revenueEst: 0
-	},
-	{
-		projectName: "Registration",
-		id: 3,
-		projectDescription: "Put your sponsors front and center with value-add opportunities during your club’s registration process this season.",
-		projectIcon: "icon-registration",
-		projectImage: "images/project-image-3.jpg",
-		projectImageDescription: "Registration Sponsorship project",
-		billingFrequency: 'Monthly',
-		additionalInfo: {},
-		projectOptions: [
-			{
-				name: 'Web page banner ad & email confirmation ad',
-				selected: false,
-				amount: '100',
-				frequency: "monthly",
-				details: [
-					'Includes: 1 Page-width web page ad with image/logo. Hyperlinks to a designated URL 1 Page-width email ad with image/logo. Hyperlinks to a designated URL.',
-					'Include a message and offer in the email text to be sent directly to our members'
-				]
-			}
-		],
-		discount: {},
-		revenueEst: 0
-	},
-];
+
 
 
